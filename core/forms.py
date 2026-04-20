5b2c955a40346f23c1782948f5d9461d9b506ed0
AhmedBradai13 <Bradaiahmedmo3@gmail.com>
2026-04-20 13:53:15 +0100

    Added login2

--- core/forms.py
+++ core/forms.py
@@ -223,3 +223,20 @@ class EditDonneurForm(forms.ModelForm):
             'ville':          'City',
             'actif':          'Available to Donate',
         }
+
+
+class LoginForm(forms.Form):
+
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Username',
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+        })
+    )
